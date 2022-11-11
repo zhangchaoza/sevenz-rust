481850530aaeb1ffededaabb11a1fbabc3b3cd09
Brian Frazho <befrazho@gmail.com>
2022-11-11 12:21:33 -0500

    added tests for delta, and copy encoding, added an additional assertion on what got decrypted, and added a rust workflow

--- tests/decompress_encrypted_tests.rs
+++ tests/decompress_encrypted_tests.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::{path::{PathBuf}, fs::{read_to_string, read}};
 use tempfile::tempdir;
 use sevenz_rust::*;
 
@@ -9,6 +9,9 @@ fn test_decompress_file_with_password(){
     source_file.push("tests/resources/encrypted.7z");
     let temp_dir = tempdir().unwrap();
     let target = temp_dir.path().to_path_buf();
+    let mut file1_path = target.clone();
+    file1_path.push("encripted/7zFormat.txt");
     let r = decompress_file_with_password(source_file, target.as_path(), "sevenz-rust".into());
-    assert!(r.is_ok())
+    assert!(r.is_ok());
+    assert!(read_to_string(file1_path).unwrap().starts_with("7z is the new archive format, providing high compression ratio."))
 }
